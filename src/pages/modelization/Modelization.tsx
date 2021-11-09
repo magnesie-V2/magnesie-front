b9688fa5203f42fee958e96d1f7069d66338b052
Adrien Mazet <adrien.mazet.56@gmail.com>
2021-11-09 09:56:16 +0100

    errorbox

--- src/pages/modelization/Modelization.tsx
+++ src/pages/modelization/Modelization.tsx
@@ -1,5 +1,5 @@
-import { AiOutlineRedo } from "react-icons/ai";
 import Donuts from "../../components/modelization/Donuts";
+import ErrorBox from "../../components/shared/ErrorBox";
 import Spinner from "../../components/shared/Spinner";
 import useModelization from "../../hooks/modelization/useModelization";
 
@@ -11,27 +11,7 @@ const Modelization = () => {
   }
 
   if (isError) {
-    return (
-      <div className="flex flex-col justify-center items-center h-3/4 sm:h-5/6 px-12">
-        <div>
-          <div className="flex justify-between items-center bg-red-500 rounded-t px-4 py-2">
-            <p className="text-white text-lg font-bold">
-              Une erreur est survenue
-            </p>
-            <button
-              onClick={() => refetch()}
-              className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded inline-flex items-center"
-            >
-              <AiOutlineRedo className="mr-3" size="26" />
-              <p>Réessayer</p>
-            </button>
-          </div>
-          <div className="flex items-center h-full border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700 text-lg">
-            <p>{error as string}</p>
-          </div>
-        </div>
-      </div>
-    );
+    return <ErrorBox error={error as string} refetch={refetch} />;
   }
 
   // change styles (height rules)
