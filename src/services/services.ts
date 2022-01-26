129a3688ad51334f323c327e35956593e0eb27da
Adrien Mazet <adrien.mazet.56@gmail.com>
2022-01-26 21:58:40 +0100

    Modelization Details

--- src/services/services.ts
+++ src/services/services.ts
@@ -54,6 +54,8 @@ export const getModelization = (modelizationID: string | undefined) => {
         name: modelizationID as string,
         modelPath: `${FOLDER}/${object}/${PLY_FILE}`,
         texturePath: `${FOLDER}/${object}/${PNG_FILE}`,
+        duration: Math.floor(Math.random() * (10000 - 1000 + 1) + 1000),
+        consumption: Math.floor(Math.random() * (150 - 50 + 1) + 50),
       });
     }, 500)
   );
@@ -67,26 +69,36 @@ export const getModelizations = () => {
           name: "castle",
           modelPath: `${FOLDER}/castle/${PLY_FILE}`,
           texturePath: `${FOLDER}/castle/${PNG_FILE}`,
+          duration: 0,
+          consumption: 0,
         },
         {
           name: "horse",
           modelPath: `${FOLDER}/horse/${PLY_FILE}`,
           texturePath: `${FOLDER}/horse/${PNG_FILE}`,
+          duration: 0,
+          consumption: 0,
         },
         {
           name: "bunny",
           modelPath: `${FOLDER}/bunny/${PLY_FILE}`,
           texturePath: `${FOLDER}/bunny/${PNG_FILE}`,
+          duration: 0,
+          consumption: 0,
         },
         {
           name: "dragon",
           modelPath: `${FOLDER}/dragon/${PLY_FILE}`,
           texturePath: `${FOLDER}/dragon/${PNG_FILE}`,
+          duration: 0,
+          consumption: 0,
         },
         {
           name: "bronze_sculpture",
           modelPath: `${FOLDER}/bronze_sculpture/${PLY_FILE}`,
           texturePath: `${FOLDER}/bronze_sculpture/${PNG_FILE}`,
+          duration: 0,
+          consumption: 0,
         },
       ]);
     }, 500)
--- src/types/Modelization.d.ts
+++ src/types/Modelization.d.ts
@@ -2,4 +2,6 @@ type Modelization = {
   name: string;
   modelPath: string;
   texturePath: string;
+  duration: number;
+  consumption: number;
 };
